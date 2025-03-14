aef5d47defb8d70e32edad3a9849233b0ee04032
Nathan <natefintak@gmail.com>
2025-03-14 11:30:24 -0400

    added inFavorOf and isTie methods

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -12,23 +12,60 @@ public class Board {
 
     public Boolean isInFavorOfX() {  //does X have more spaces filled in? is it X's turn?
         Boolean xInFavor = false;
-
-
+        int numOfXs = 0;
+        int numOfOs = 0;
+        for (int i = 0; i < matrix.length; i++) { //moves thru rows
+            for (int j = 0; j < matrix[i].length; j++) { //moves thru columns
+                if (matrix[i][j] == 'X') {
+                    numOfXs++;
+                } else if (matrix[i][j] == 'O') {
+                    numOfOs++;
+                }
+            }
+        }
+        if (numOfXs > numOfOs && numOfXs + numOfOs != 9) {
+            xInFavor = true;
+        }
         return xInFavor;
         //return null;
     }
 
     public Boolean isInFavorOfO() {  //does O have more spaces filled in? is it O's turn?
         Boolean oInFavor = false;
-
-
+        int numOfXs = 0;
+        int numOfOs = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 'X') {
+                    numOfXs++;
+                } else if (matrix[i][j] == 'O') {
+                    numOfOs++;
+                }
+            }
+        }
+        if (numOfOs > numOfXs && numOfXs + numOfOs != 9) {
+            oInFavor = true;
+        }
         return oInFavor;
         //return null;
     }
 
     public Boolean isTie() { //no spaces are equal to ' '
         Boolean tied = false;
-
+        int numOfXs = 0;
+        int numOfOs = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 'X') {
+                    numOfXs++;
+                } else if (matrix[i][j] == 'O') {
+                    numOfOs++;
+                }
+            }
+        }
+        if (numOfXs + numOfOs == 9) {
+            tied = true;
+        }
         return tied;
         //return null;
     }
